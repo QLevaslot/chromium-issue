4a617a3a4fff598e1b9f30177b497122a12293c7
qlevaslot <quentin.levaslot@gmail.com>
2024-10-30 10:53:11 +0100

    cleanup

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,33 +1,20 @@
-import {ApplicationConfig, importProvidersFrom, provideZoneChangeDetection, isDevMode} from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import {
-    HttpEvent, HttpHandlerFn,
-    HttpRequest, HttpResponse,
     provideHttpClient,
-    withInterceptors,
-    withInterceptorsFromDi
 } from "@angular/common/http";
 import { provideServiceWorker } from '@angular/service-worker';
-import {Observable} from "rxjs";
 
 export const appConfig: ApplicationConfig = {
   providers: [
       provideZoneChangeDetection({ eventCoalescing: true }),
       provideRouter(routes),
-      provideHttpClient(withInterceptors([inspectionDataInterceptor$]), withInterceptorsFromDi()),
+      provideHttpClient(),
       provideServiceWorker('ngsw-worker.js', {
             enabled: true,
             registrationStrategy: 'registerWhenStable:30000'
           })
   ]
 };
-
-export function inspectionDataInterceptor$(
-    request: HttpRequest<unknown>,
-    next: HttpHandlerFn
-): Observable<HttpEvent<unknown>> | Observable<HttpResponse<undefined>> {
-    console.log('request', request);
-    return next(request);
-}
